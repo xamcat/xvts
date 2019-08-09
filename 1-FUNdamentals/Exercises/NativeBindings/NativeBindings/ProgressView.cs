8361f1dd2775ba5375351c915bd84922ab083dcc
DeanFaizal <2334530+DeanFaizal@users.noreply.github.com>
2019-08-09 10:49:34 -0400

    Native bindings (#3)
    Native projects, binding projects, & forms consumer app

---
+++ 1-FUNdamentals/Exercises/NativeBindings/NativeBindings/ProgressView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NativeBindings
+{
+    public class ProgressView : View
+    {
+        public static readonly BindableProperty IsDisplayedProperty = BindableProperty.Create(nameof(IsDisplayed),
+            typeof(bool),
+            typeof(ProgressView),
+            default(bool));
+
+        public bool IsDisplayed
+        {
+            get { return (bool)GetValue(IsDisplayedProperty); }
+            set { SetValue(IsDisplayedProperty, value); }
+        }
+
+
+        public static readonly BindableProperty ProgressValueProperty = BindableProperty.Create(nameof(ProgressValue),
+            typeof(float),
+            typeof(ProgressView),
+            default(float));
+
+        public float ProgressValue
+        {
+            get { return (float)GetValue(ProgressValueProperty); }
+            set { SetValue(ProgressValueProperty, value); }
+        }
+    }
+}
